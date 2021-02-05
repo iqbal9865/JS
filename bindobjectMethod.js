b8327e39b2baa8f32b7ff9154dbabdd2c3ffff01
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-02-05 20:30:56 +0600

    added another

--- bindobjectMethod.js
+++ bindobjectMethod.js
@@ -37,3 +37,44 @@ console.log(hero.salary);
 const singerBill = myInfo.getCharge.bind(singer);
 singerBill(12234);
 singerBill(424556);
+
+
+
+
+//another example of bind object method
+
+const student = {
+    // firstName: 'A',
+    // lastName: 'B',
+    getFullName: function(firstName,lastName){
+        console.log(this)
+        return firstName + ' ' + lastName;
+    },
+    tuitionFee: 17200,
+    payTuitionFee: function(amount){
+        console.log(this);
+        this.tuitionFee = this.tuitionFee - amount;
+    }
+}
+const student1 = {
+    firstName: 'Iqbal',
+    lastName: 'Ahmed',
+    tuitionFee:17200
+}
+const student2 = {
+    firstName: 'Alvi',
+    lastName: 'Hasan',
+    tuitionFee:17200
+}
+const student3 = {
+    firstName: 'Jakaria',
+    lastName: 'Jiku',
+    tuitionFee:17200
+}
+const iqbalInfo = student.payTuitionFee.bind(student1);
+const iqbalName = student.getFullName.bind(student2);
+iqbalInfo(4000);
+iqbalInfo(2000);
+iqbalInfo(2000);
+const storeName = iqbalName('iqbal', 'Ahmed');
+console.log(storeName);
