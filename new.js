908e27fc822ff6f260282888be772b4ad2af957c
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-02-05 22:07:42 +0600

    added new

---
+++ new.js
@@ -0,0 +1,11 @@
+class person {
+    constructor(firstName,lastName,salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+const person1 = new person('Iqbal', 'Ahmed', 18000);
+console.log(person1);
+const person2 = new person('Nahid', 'Hasan', 20000);
+console.log(person2);
