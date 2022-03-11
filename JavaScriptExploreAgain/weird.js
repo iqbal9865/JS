3eaa1dd96430a9e5e4234b8f04e50e2968f25bfb
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2022-03-11 15:59:57 +0600

    JavaScript Number Methods

--- JavaScriptExploreAgain/weird.js
+++ JavaScriptExploreAgain/weird.js
@@ -22,6 +22,31 @@ console.log(undefined === undefined)  //undefined === undefined returns true
 // then why NaN === NaN is returns false? Why? 
 
 
-
+// BtW From here this is not weird thing in JS. I just lazy to create another file.
 // const allPera = 'Bangladesh is My country';
 // console.log(allPera.slice(-10));
+
+const textOne = "           Emran Hasan         ";
+console.log('length of textone: '+ textOne.length);
+const textTwo = textOne.trim();
+console.log("length of text two: "+ textTwo.length);
+
+
+const country = 'Bangladesh';
+const population = 180000000;
+const combined = `Our country name is ${country}. it has a huge population over ${population}`;
+console.log(combined);
+
+// const number = 999999999999999466656;
+// console.log(number)
+// JavaScript variable can store maximum 15 integer digits and 17 floting digits, then it unable to remember the numbers.
+
+// let x = 123;
+// let y = x.toString();
+// console.log(typeof y)
+
+// let w = 123;
+// let z = w.valueOf();
+// console.log(typeof w);
+
+// console.log(typeof Number(false));
